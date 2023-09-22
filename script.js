0a0917b174abb1bfc6dd67688c56cf5e957c89c9
desireekodehode <desireekodehode1@gmail.com>
2023-09-22 08:41:01 +0200

    ***

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+// function randomNumber () {  
+
+
+
+
+//  const numbers = [
+//      "En", 
+//      "To",
+//       "Tre",
+//       "Fire",
+//       "Fem",
+//       "Seks"
+//     ];
+ 
+// const randomIndex = Math.floor(Math.random()* numbers.length)
+// const number = number[randomIndex];
+// return numbers;
+// console.log(numbers)
+
+// const button =  document.getElementById("click");
+// let outPut = document.getElementById("randomNumbersOutPut");
+// outPut.textContent = numbers
+// console.log(numbers)
+//   }
+
+// const numberlist = document.createElement("ul");
+
+// for (let i = 0; i < numbers.length; i++){
+
+//   const numbers = document.createElement();
+  
+// }
+// return randomNumber()
+// }
+
+
+//  const newElement = document.appendChild("Hallo");
+
+//  function randomNumberGenerator() {
+//   let randomNumber = Math.floor((Math.random() * 1000) + 1)
+//   return randomNumber
+// }
+
+// document.getElementById("randomNumber").textContent = "Your lucky number of the day is: " + randomNumberGenerator()
+
+//  console.log()
+
+ function hils() {
+  document.getElementById("jsOutput").textContent = "Hurra!"
+}
+function hilse() {
+  document.getElementById("jsOutput").textContent = "JIPPI!"
+}
+
